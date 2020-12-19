681d79fea99b32e232c5061d21d6a6a5652a067a
tonem123@naver.com <tonem123@naver.com>
2020-12-19 18:08:53 +0900

    Create instance model

--- uplex/settings.py
+++ uplex/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'lens.apps.LensConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
