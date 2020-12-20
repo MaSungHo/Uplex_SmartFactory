13da7ebb624cdcda959034f3c8cc8f059d339029
tonem123@naver.com <tonem123@naver.com>
2020-12-20 18:01:58 +0900

    Print model info on page

--- lens/urls.py
+++ lens/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('detail/<int:instance_id>/', views.detail, name='detail'),
+    path('results/<int:instance_id>/', views.results, name='results'),
 ]
--- lens/views.py
+++ lens/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Instance
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the uplex index page.")
+    inst = Instance.objects.first()
+    return HttpResponse(inst.trt_lot_no)
+
+def detail(request, instance_id):
+    return HttpResponse("You're looking at instance %i" %instance_id)
+
+def results(request, instance_id):
+    response = "You're looking at the results of instance %i"
+    return HttpResponse(response %instance_id)
