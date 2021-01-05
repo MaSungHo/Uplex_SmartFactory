b2ed840bd31fffcc9f39904f04b3d37d0d481e5f
MaSungHo <tonem123@naver.com>
2021-01-05 11:40:40 +0900

    Finish django
    Former-commit-id: 22ca363bd0bcce977455395f66326a79fa20de0b

--- lens/views.py
+++ lens/views.py
@@ -15,12 +15,13 @@ from pathlib import Path
 from influxdb import InfluxDBClient
 import os, json, requests
 from django.core.exceptions import ImproperlyConfigured
+from sklearn.decomposition import PCA
 from sklearn.metrics import accuracy_score
 from sklearn import svm
 from sklearn.model_selection import GridSearchCV
-from sklearn.decomposition import PCA
 from sklearn.svm import SVC
-
+from tensorflow.keras.models import load_model
+from math import hypot
 
 from .models import Instance
 
@@ -54,6 +55,50 @@ def blacking(img) :
       if math.hypot(i - x, j - y) > r:
         img[j, i] = 0
   return img
+
+@jit
+def preprocess_sample(samples):
+    samples = np.asarray(samples)
+    mult = samples[0].shape[0] * samples[0].shape[1]
+
+    samples = samples.reshape(len(samples), mult)
+    samples = samples.astype('float32')
+    samples = samples / 255.0 * 100
+    
+    pca = PCA(n_components=70)
+    samples = pca.fit_transform(samples)
+    
+    return samples
+
+@jit
+def old_mask(img):
+    x_start = 50
+    y_start = 50
+    crop_img = img[y_start:y_start + 1255, x_start:x_start + 1250]
+    #                 RESIZE CROPPED
+    scale_percent = 50 #was 50. can't detect circles at 25%
+    width = int(crop_img.shape[1] * scale_percent / 100)
+    height = int(crop_img.shape[0] * scale_percent / 100)
+    dsize = (width, height)
+    img = cv2.resize(crop_img, dsize)
+
+    img = cv2.medianBlur(img,5)
+    cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+    circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 400, param1=50, param2=25, minRadius=150, maxRadius=0)
+    circles = np.uint16(np.around(circles))
+
+    for i in circles[0, :]:
+        cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0) ,2)
+        cv2.circle(cimg, (i[0], i[1]), 2, (0 ,0, 255), 3)
+
+    x, y, r = circles[0, :][0]
+    rows, cols = img.shape
+
+    for i in range(cols):
+        for j in range(rows):
+            if hypot(i - x, j - y) > r:
+                img[j, i] = 0
+    return img
 
 # Create your views here.
 def index(request):
@@ -97,7 +142,7 @@ def results(request):
   }
 
   res = requests.post(get_secret("API_PATH"), headers = headers, data = json.dumps(params)).json()
-  prediction = res['predicted'][0] * 100
+  prediction = res['predicted'][0][0] * 100
 
   return render(request, 'lens/result.html', {'prediction': prediction})
 
@@ -138,28 +183,53 @@ def decision(request):
     raise Http404('올바르지 않은 요청입니다.')
 
 def decisions(request):
+  model_type = request.POST['model_type']
   images = []
   preprocess_images = []
   
-  if request.method == 'POST':
-    images = [Image(image = img) for img in request.FILES.getlist('images')]
-    Image.objects.bulk_create(images)
+  if model_type == "SVM":
+    if request.method == 'POST':
+      images = [Image(image = img) for img in request.FILES.getlist('images')]
+      Image.objects.bulk_create(images)
 
-    for image in images:
-      img = cv2.imread(image.image.path, 0)
-      img = blacking(img)
-      img = cv2.resize(img, (512,512))
-      img = img.reshape(512 * 512)
-      preprocess_images.append(img)
+      for image in images:
+        img = cv2.imread(image.image.path, 0)
+        img = blacking(img)
+        img = cv2.resize(img, (512,512))
+        img = img.reshape(512 * 512)
+        preprocess_images.append(img)
 
-    pca = joblib.load(os.path.join(settings.MEDIA_ROOT, 'PCA.pkl'))
-    preprocess_images = pca.transform(preprocess_images)  
-    svm = joblib.load(os.path.join(settings.MEDIA_ROOT, 'SVM_Model.pkl'))
-    results = svm.predict(preprocess_images)
+      pca = joblib.load(os.path.join(settings.MEDIA_ROOT, 'PCA.pkl'))
+      preprocess_images = pca.transform(preprocess_images)  
+      svm = joblib.load(os.path.join(settings.MEDIA_ROOT, 'SVM_Model.pkl'))
+      results = svm.predict(preprocess_images)
+    
+      predictions = zip(images, results)
+      
+  else:
+    if request.method == 'POST':
+      images = [Image(image = img) for img in request.FILES.getlist('images')]
+      Image.objects.bulk_create(images)
 
-   
+      for image in images:
+        img = cv2.imread(image.image.path, 0)
+        img = old_mask(img)
+        preprocess_images.append(img)
 
-    return render(request, 'lens/group_decision.html', {'results': results})
+      new_model = load_model(settings.MEDIA_ROOT, custom_objects=None, compile=True)
+      pred_ready_x = preprocess_sample(preprocess_images)
+      prediction = new_model.predict(pred_ready_x)
+      
+      results = []
+      for pred in prediction:
+        if (pred[0] > pred[1]):
+          results.append(0)
+        elif(pred[0] < pred[1]):
+          results.append(1)
+
+      predictions = zip(images, results)
+  return render(request, 'lens/group_decision.html', {'predictions': predictions})
+ 
 
 def search(request):
   client = InfluxDBClient(host=get_secret("DB_HOST"), port=get_secret("DB_PORT"), username=get_secret("DB_USERNAME"),
